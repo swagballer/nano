3351e7379f6a2a82674ae9df7a0a087d53a32b59
swagballer <szhao66@illinois.edu>
2024-12-29 19:36:02 -0600

    testing out avatars

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,22 @@
-// Select all necessary elements
 const gridItems = document.querySelectorAll('.grid-item');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 const videoElement = modal.querySelector('video');
 const closeButton = modal.querySelector('.close-button');
 
-// Function to open modal
 function openModal(videoSrc) {
   videoElement.src = videoSrc;
   modal.style.display = 'block';
   overlay.style.display = 'block';
 }
 
-// Function to close modal
 function closeModal() {
   videoElement.pause();
-  videoElement.src = ''; // Clear video source
+  videoElement.src = '';
   modal.style.display = 'none';
   overlay.style.display = 'none';
 }
 
-// Add event listeners to grid items
 gridItems.forEach(item => {
   item.addEventListener('click', () => {
     const videoSrc = item.getAttribute('data-video');
@@ -28,6 +24,5 @@ gridItems.forEach(item => {
   });
 });
 
-// Add event listener to close button and overlay
 closeButton.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
